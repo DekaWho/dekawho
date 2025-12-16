8a2aab9c48ad66052a2fc7855c15ec7a17c3f632
Dekawho <11019529+DekaWho@users.noreply.github.com>
2025-12-16 17:07:14 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -90,7 +90,7 @@ async function startTypewriter() {
   await typeWriter(p3, "¿Y si nosotros somos los monos? No hablo de conspiraciones. No hay un grupo de señores gordos y forrados en una sala oscura decidiendo qué va a ser de tu vida. Pero puede ser peor que eso...", 50);
   await typeWriter(p4, "¿Crees que un título universitario te hace más válido?<br><br>¿Crees que la estabilidad te da libertad?<br><br>¿Crees que está mal querer más de lo que tienes?<br><br>¿Crees que todos los ricos son malas personas?<br><br>No juzgo tus respuestas. Es más, no creas nada de lo que digo. Construye tus propias conclusiones.", 50);
   await typeWriter(p6, "Yo solo busco las mejores preguntas. Toda la vida, desde que fui un niño preguntón dando la turra. El problema hoy en día es que la dopamina rápida y el scroll infinito son peor que un manguerazo de agua fría. Por eso esto no encaja en ningún feed.", 50); 
-  await typeWriter(p7, "Si quieres seguir tu propio camino aunque no sea lo que siempre se ha hecho: Rompo mitos en cada uno de mis emails. Los tienes gratis en tu bandeja de entrada.", 70);
+  await typeWriter(p7, "Si prefieres preguntas incómodas a respuestas fáciles, este es tu sitio. Mis vídeos en redes agrietan el muro pero mis emails lo derriban. Gratis en tu bandeja de entrada.", 70);
 
 
   await new Promise(resolve => setTimeout(resolve, 1000)); // Delay de 1s
