54e267aa0fa8c83df98112ea912d3ae96012ade9
Dekawho <11019529+DekaWho@users.noreply.github.com>
2025-12-27 02:43:26 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -86,13 +86,13 @@ async function startTypewriter() {
   const p8 = document.querySelector(".p8");
   const p9 = document.querySelector(".p9");
 
-/*   await typeWriter(t1, "[experimento_1498]", 120);
+  await typeWriter(t1, "[experimento_1498]", 120);
   await typeWriter(p1, "Hay 5 monos en una jaula y una escalera con plátanos arriba. En cuanto un mono intenta subir les dan a todos un manguerazo de agua fría. Tras unos intentos ninguno se acerca a la escalera.", 70);
   await typeWriter(p2, "Ahora los investigadores sacan a un mono y meten otro nuevo. Este va hacia la escalera pero el resto lo frena a golpes. Van cambiando todos los monos. Ya ninguno ha sufrido el manguerazo pero siguen dando palizas al que se acerca a la escalera. Es como siempre lo han hecho.", 50);
   await typeWriter(p3, "¿Y si nosotros somos los monos?", 50);
   await typeWriter(p4, "No hablo de conspiraciones. No hay un grupo de señores gordos y forrados en una sala oscura decidiendo sobre tu vida. Pero puede ser peor que eso...", 50);
   await typeWriter(p5, "¿Crees que un título universitario te hace más válido?<br><br>¿Está mal querer más de lo que tienes?<br><br>¿Todos los ricos son malas personas?<br><br>No juzgo tus respuestas. Ni yo sé si tengo razón. Es más, no creas nada de lo que digo.", 50);
-  await typeWriter(p6, "Yo solo busco las mejores preguntas. Toda la vida, desde que fui un niño preguntón. El problema es que la dopamina rápida y el scroll infinito son peor que un manguerazo de agua fría. Por eso esto no encaja en ningún feed.", 50); */ 
+  await typeWriter(p6, "Yo solo busco las mejores preguntas. Toda la vida, desde que fui un niño preguntón. El problema es que la dopamina rápida y el scroll infinito son peor que un manguerazo de agua fría. Por eso esto no encaja en ningún feed.", 50);
 
 
   const hasTouch = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
